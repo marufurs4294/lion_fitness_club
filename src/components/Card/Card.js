f9156ba8888bf685d545ccde0f2651bf3480793c
marufurs4294 <siyamm4294@gmail.com>
2022-12-27 12:26:03 +0600

    card component added

---
+++ src/components/Card/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Card.css'
+
+const Card = (props) => {
+    const {img, title, description, age, time} = props.fit;
+    return (
+        <div className="col mx-0 my-4">
+        <div className="card h-100 px-4 pt-4 pb-2">
+          <img
+            src={img}
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body text-start pt-3 pb-0">
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text text-muted">{description.slice(163, )}</p>
+            <p>For Age: <span className='fw-bold'>{age}</span> </p>
+            <p>Time Required: <span className='fw-bold'>{time} Minutes</span></p>
+          </div>
+        </div>
+      </div>
+    
+    );
+};
+
+export default Card;
--- src/components/Fitness/Fitness.js
+++ src/components/Fitness/Fitness.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+import './Fitness.css'
 
 const Fitness = () => {
     const [fitness, setFitness] = useState([]);
@@ -10,7 +12,25 @@ const Fitness = () => {
     },[])
     return (
         <div>
-            
+            <div className='fitness m-5'>
+            <div className="row flex-sm-column-reverse flex-lg-row">
+            <div className="col-sm-12 col-md-9 col-lg-9">
+            <div className='text-start'>
+                <h2 className='fw-bold club-name'> <i class="fa-brands fa-wolf-pack-battalion fs-1"></i> LION FITNESS CLUB</h2>
+                <h4 className='mt-4'>Select today's activities</h4>
+            </div>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                
+            {
+                    fitness.map(fit => <Card key={fit.id} fit={fit}></Card>)
+                }
+                </div>
+                
+            </div>
+            <div className="col-sm-12 col-md-3 col-lg-3">
+            </div>
+            </div>
+            </div>
         </div>
     );
 };
